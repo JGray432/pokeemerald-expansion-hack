dabd0fb2be4b3c510a4c15ee758824f8eccd2647
JGray432 <jwgray432@gmail.com>
2025-07-11 23:22:51 +0100

    Some utility scientist fixes, candy man, tm ball graphics, gem boosts gen 5

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -47,8 +47,8 @@
 #define FLAG_ROUTE_6_BACKTOBACK    0x21 
 #define FLAG_DELIVERED_THREAD    0x22 
 #define FLAG_NO_BAG_USE_IN_BATTLE    0x23 
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_RECEIVED_INF_CANDY    0x24 // Unused Flag
+#define FLAG_RECEIVED_CAP_CANDY    0x25 // Unused Flag
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
---
+++ include/utilities_scientist.h
@@ -0,0 +1,6 @@
+#ifndef GUARD_DATA_UTILITIES_SCIENTIST_H
+#define GUARD_DATA_UTILITIES_SCIENTIST_H
+
+void ApplyStatusConditionToPartyMon(void);
+void MaximizeIVOfChosenMon(void);
+#endif // GUARD_DATA_UTILITIES_SCIENTIST_H
